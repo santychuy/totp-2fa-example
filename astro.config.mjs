adb5377110055da4a4181e95ba592b5ca9c558c6
Jesus Santiago Carrasco Campa <jsanticc@hotmail.com>
2024-07-12 17:27:49 -0700

    build: setup astro config to ssr and adapt to use astro db

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,24 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  security: {
+    checkOrigin: true,
+  },
   integrations: [
     react(),
     tailwind({
       applyBaseStyles: false,
     }),
+    db(),
   ],
+  vite: {
+    optimizeDeps: {
+      exclude: ["astro:db"],
+    },
+  },
 });
